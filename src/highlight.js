1fce22ed86d246ce27cdd0a16ae94c337ac06a13
Sean Sullivan <ssullivan61198@gmail.com>
2023-01-30 14:20:03 -0800

    fix highlighting

--- src/highlight.js
+++ src/highlight.js
@@ -1,12 +1,15 @@
 import {styleTags, tags as t} from "@lezer/highlight"
 
-export const bashHighlight = styleTags({
-  "while do done until for in case esac if elif else fi": t.controlKeyword,
+export const bashHighlighting = styleTags({
+  "while do done until for in case esac if then elif else fi": t.controlKeyword,
   IORedirect: t.operator,
-  "&& || |": t.logicalOperator,
+  "&& || |": t.logicOperator,
+	"= +=": t.operator,
   "( )": t.paren,
   "[ ]": t.squareBracket,
   "{ }": t.brace,
+	"${ }": t.brace,
+	"$( )": t.paren,
   RawString: t.string,
   String: t.string,
   AnsiCString: t.string,
@@ -15,5 +18,5 @@ export const bashHighlight = styleTags({
   FunctionName: t.variableName,
   Comment: t.comment,
   CommandName: t.name,
-  "& ;": t.separator,
+  "; &": t.separator,
 })
